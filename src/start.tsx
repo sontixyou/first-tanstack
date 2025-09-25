4e633545fa97b630a0adc96884971e6641e06daa
sontixyou <kengoviolinphoto@gmail.com>
2025-09-25 23:38:50 +0900

    add first pages

---
+++ src/start.tsx
@@ -0,0 +1,12 @@
+// src/start.tsx
+import { createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+
+export function getRouter() {
+  const router = createRouter({
+    routeTree,
+    scrollRestoration: true,
+  })
+
+  return router
+}

4b1530e64ce6b10fcff3ba8bf5099d208dc9a2a1
sontixyou <kengoviolinphoto@gmail.com>
2025-09-26 00:08:25 +0900

    change calculation process

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import * as fs from 'node:fs'
 import { createFileRoute, useRouter } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/react-start'
 
-const filePath = 'count.txt'
+const filePath = './src/routes/count.txt'
 
 async function readCount() {
   return parseInt(
@@ -21,7 +21,10 @@ const updateCount = createServerFn({ method: 'POST' })
   .inputValidator((d: number) => d)
   .handler(async ({ data }) => {
     const count = await readCount()
+
     await fs.promises.writeFile(filePath, `${count + data}`)
+
+    return count + data
   })
 
 export const Route = createFileRoute('/')({
@@ -36,10 +39,14 @@ function Home() {
   return (
     <button
       type="button"
-      onClick={() => {
-        updateCount({ data: 1 }).then(() => {
+      onClick={async () => {
+        try {
+          await updateCount({ data: 1 })
+
           router.invalidate()
-        })
+        } catch (error) {
+          console.error('Error updating count:', error)
+        }
       }}
     >
       Add 1 to {state}?
--- src/start.tsx
+++ src/start.tsx
@@ -1,5 +1,6 @@
 // src/start.tsx
 import { createRouter } from '@tanstack/react-router'
+import { createStart } from '@tanstack/react-start'
 import { routeTree } from './routeTree.gen'
 
 export function getRouter() {
@@ -9,4 +10,16 @@ export function getRouter() {
   })
 
   return router
+}
+
+export const startInstance = createStart(() => {
+  return {
+    requestMiddleware: [],
+  }
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: ReturnType<typeof getRouter>
+  }
 }
